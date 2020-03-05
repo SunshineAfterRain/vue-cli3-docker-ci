8a0a8b5a94dde4a9d3d8eea5e8694ed0b3c3f5aa
tanguopeng <guopeng.tan@dreampix.cn>
2020-03-05 22:14:12 +0800

    update

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,11 +1,11 @@
 <template>
   <div @click="handle">
-      <img
+    <img
       alt="Vue logo"
       src="../assets/img/logo.png"
-    > 
+    >
     <canvas id="canvas"></canvas>
-    <cc-button>aaa</cc-button>
+    <cc-button>bbb</cc-button>
   </div>
 </template>
 
@@ -14,38 +14,38 @@ export default {
   name: 'Index',
   components: {
   },
-  methods:{
-    handle(e){  
+  methods: {
+    handle(e) {
       console.log(e)
     }
   },
-  mounted(){
+  mounted() {
     console.log(document.getElementById('canvas'))
     var ctx = document.getElementById('canvas'),
-        content = ctx.getContext('2d'),
-        WIDTH,
-        HEIGHT,
-        round = [],
-        initRoundPopulation = 80;
+      content = ctx.getContext('2d'),
+      WIDTH,
+      HEIGHT,
+      round = [],
+      initRoundPopulation = 80;
 
     WIDTH = document.documentElement.clientWidth;
     HEIGHT = document.documentElement.clientHeight;
     ctx.width = WIDTH
     ctx.height = HEIGHT
-    
 
 
-    function round_item(index,x,y) {
+
+    function round_item(index, x, y) {
       this.index = index
       this.x = x
       this.y = y
       this.r = Math.random() * 2 + 1
-      var alpha = (Math.floor(Math.random()*10) + 1)/10/2
+      var alpha = (Math.floor(Math.random() * 10) + 1) / 10 / 2
       this.color = `rgba(255,255,255,${alpha})`
     }
 
     round_item.prototype.draw = function () {
-      content.fillStyle =  this.color
+      content.fillStyle = this.color
       content.shadowBlur = this.r * 2;
       content.beginPath();
       content.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
@@ -55,15 +55,15 @@ export default {
 
     round_item.prototype.move = function () {
       this.y -= 0.15
-      if(this.y <= -20) {
+      if (this.y <= -20) {
         this.y = HEIGHT + 10
-      } 
+      }
       this.draw()
     }
-    
+
     function init() {
       for (let index = 0; index < initRoundPopulation; index++) {
-        round[index] = new round_item(index,Math.random()*WIDTH, Math.random()*HEIGHT)
+        round[index] = new round_item(index, Math.random() * WIDTH, Math.random() * HEIGHT)
 
         round[index].draw()
       }
@@ -71,15 +71,15 @@ export default {
     }
 
     function animate() {
-      content.clearRect(0,0,WIDTH,HEIGHT)
-      for(var i in round) {
+      content.clearRect(0, 0, WIDTH, HEIGHT)
+      for (var i in round) {
         round[i].move();
 
       }
-      window.requestAnimationFrame(animate  )
+      window.requestAnimationFrame(animate)
     }
     init()
-    
+
   }
 }
 </script>
